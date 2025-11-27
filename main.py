e303c092f30c5b9f011e38500da3a61f0cf30de3
Athos <fathos82@gmail.com>
2025-11-27 15:51:35 -0300

    html response

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 async def root():
-    return  "<h1>Eai Galera do Curso do Athos!</h1>"
+    return "<h1>Eai Galera do Curso do Athos!</h1>"
 
-
-@app.get("/hello/{name}")
+@app.get("/hello/{name}", response_class=HTMLResponse)
 async def say_hello(name: str):
     return f"<h1>hello! {name}</h1>"
